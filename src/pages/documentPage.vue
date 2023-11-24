d417e4e62ef5adc84b23e018b2812b537e33b085
horuixiang98 <horuixiang@gmail.com>
2023-11-24 08:41:33 +0800

    first commit

---
+++ src/pages/documentPage.vue
@@ -0,0 +1,325 @@
+<script setup>
+import csvimg from '@images/images/csv.png'
+import { useTheme } from 'vuetify'
+
+// Components
+
+const vuetifyTheme = useTheme()
+
+
+const sheet = ref(false)
+
+// const props = defineProps({
+  
+//   data: {
+//     sheet: false,
+//   },
+  
+// })
+</script>
+
+<template>
+  <VRow>
+    <VSpacer />
+    <VBtn
+      color="primary"
+      v-bind="props"
+      @click="sheet = !sheet"
+    >
+      Upload Document
+    </VBtn>
+  </VRow>
+  <div>
+    <VContainer>
+      <VRow>
+        <VChip class="mb-3 mt-6">
+          <p class="text-title ma-5">
+            Recent Document
+          </p>
+        </VChip>
+      </VRow>
+      <VRow>
+        <VCard
+          class="elevation-0 ma-1"
+          style="max-width: 10%; max-height: 180px; min-width: 95px; border-radius: 5%; opacity: 0.8;"
+        >
+          <VImg
+            class="ma-3"
+            max-width="100"
+            :src="csvimg"
+          />
+          <p class="text-center text-caption px-2">
+            ASerious_OCT_2023_Sales_Report
+          </p>
+        </VCard>
+        <VCard
+          class="elevation-0 ma-1"
+          style="max-width: 10%; max-height: 180px; min-width: 95px; border-radius: 5%; opacity: 0.8;"
+        >
+          <VImg
+            class="ma-3"
+            max-width="100"
+            :src="csvimg"
+          />
+          <p class="text-center text-caption px-2">
+            ASerious_SEPT_2023_Sales_Report
+          </p>
+        </VCard>
+
+        <VCard
+          class="elevation-0 ma-1"
+          style="max-width: 10%; max-height: 180px; min-width: 95px; border-radius: 5%; opacity: 0.8;"
+        >
+          <VImg
+            class="ma-3"
+            max-width="100"
+            :src="csvimg"
+          />
+          <p class="text-center text-caption px-2">
+            ASerious_AUG_2023_Sales_Report
+          </p>
+        </VCard>
+      </VRow>
+      <VContainer />
+      <!-- <VDivider /> -->
+      <!-- Submitted Document Docs -->
+      <VRow>
+        <VChip class="mb-3 mt-6">
+          <p class="text-title ma-5">
+            Submitted Document
+          </p>
+        </VChip>
+      </VRow>
+
+      <VRow>
+        <VCard
+          class="elevation-0 ma-1"
+          style="max-width: 10%; max-height: 180px; min-width: 95px; border-radius: 5%; opacity: 0.8;"
+        >
+          <VImg
+            class="ma-3"
+            max-width="100"
+            :src="csvimg"
+          />
+          <p class="text-center text-caption px-2">
+            ASerious_OCT_2023_Sales_Report
+          </p>
+        </VCard>
+        <VCard
+          class="elevation-0 ma-1"
+          style="max-width: 10%; max-height: 180px; min-width: 95px; border-radius: 5%; opacity: 0.8;"
+        >
+          <VImg
+            class="ma-3"
+            max-width="100"
+            :src="csvimg"
+          />
+          <p class="text-center text-caption px-2">
+            ASerious_SEPT_2023_Sales_Report
+          </p>
+        </VCard>
+
+        <VCard
+          class="elevation-0 ma-1"
+          style="max-width: 10%; max-height: 180px; min-width: 95px; border-radius: 5%; opacity: 0.8;"
+        >
+          <VImg
+            class="ma-3"
+            max-width="100"
+            :src="csvimg"
+          />
+          <p class="text-center text-caption px-2">
+            ASerious_AUG_2023_Sales_Report
+          </p>
+        </VCard>
+
+        <VCard
+          class="elevation-0 ma-1"
+          style="max-width: 10%; max-height: 180px; min-width: 95px; border-radius: 5%; opacity: 0.8;"
+        >
+          <VImg
+            class="ma-3"
+            max-width="100"
+            :src="csvimg"
+          />
+          <p class="text-center text-caption px-2">
+            ASerious_JUL_2023_Sales_Report
+          </p>
+        </VCard>
+
+        <VCard
+          class="elevation-0 ma-1"
+          style="max-width: 10%; max-height: 180px; min-width: 95px; border-radius: 5%; opacity: 0.8;"
+        >
+          <VImg
+            class="ma-3"
+            max-width="100"
+            :src="csvimg"
+          />
+          <p class="text-center text-caption px-2">
+            ASerious_JUN_2023_Sales_Report
+          </p>
+        </VCard>
+        
+        <VCard
+          class="elevation-0 ma-1"
+          style="max-width: 10%; max-height: 180px; min-width: 95px; border-radius: 5%; opacity: 0.8;"
+        >
+          <VImg
+            class="ma-3"
+            max-width="100"
+            :src="csvimg"
+          />
+          <p class="text-center text-caption px-2">
+            ASerious_About_US
+          </p>
+        </VCard>
+
+        <VCard
+          class="elevation-0 ma-1"
+          style="max-width: 10%; max-height: 180px; min-width: 95px; border-radius: 5%; opacity: 0.8;"
+        >
+          <VImg
+            class="ma-3"
+            max-width="100"
+            :src="csvimg"
+          />
+          <p class="text-center text-caption px-2">
+            ASerious_Human_Resource_2
+          </p>
+        </VCard>
+
+        <VCard
+          class="elevation-0 ma-1"
+          style="max-width: 10%; max-height: 180px; min-width: 95px; border-radius: 5%; opacity: 0.8;"
+        >
+          <VImg
+            class="ma-3"
+            max-width="100"
+            :src="csvimg"
+          />
+          <p class="text-center text-caption px-2">
+            ASerious_Human_Resource
+          </p>
+        </VCard>
+
+        <VCard
+          class="elevation-0 ma-1"
+          style="max-width: 10%; max-height: 180px; min-width: 95px; border-radius: 5%; opacity: 0.8;"
+        >
+          <VImg
+            class="ma-3"
+            max-width="100"
+            :src="csvimg"
+          />
+          <p class="text-center text-caption px-2">
+            ASerious_MAY_2023_Sales_Report
+          </p>
+        </VCard>
+
+        <VCard
+          class="elevation-0 ma-1"
+          style="max-width: 10%; max-height: 180px; min-width: 95px; border-radius: 5%; opacity: 0.8;"
+        >
+          <VImg
+            class="ma-3"
+            max-width="100"
+            :src="csvimg"
+          />
+          <p class="text-center text-caption px-2">
+            ASerious_APR_2023_Sales_Report
+          </p>
+        </VCard>
+        
+        <VCard
+          class="elevation-0 ma-1"
+          style="max-width: 10%; max-height: 180px; min-width: 95px; border-radius: 5%; opacity: 0.8;"
+        >
+          <VImg
+            class="ma-3"
+            max-width="100"
+            :src="csvimg"
+          />
+          <p class="text-center text-caption px-2">
+            ASerious_MAR_2023_Sales_Report
+          </p>
+        </VCard>
+        <VCard
+          class="elevation-0 ma-1"
+          style="max-width: 10%; max-height: 180px; min-width: 95px; border-radius: 5%; opacity: 0.8;"
+        >
+          <VImg
+            class="ma-3"
+            max-width="100"
+            :src="csvimg"
+          />
+          <p class="text-center text-caption px-2">
+            ASerious_FEB_2023_Sales_Report
+          </p>
+        </VCard>
+
+        <VCard
+          class="elevation-0 ma-1"
+          style="max-width: 10%; max-height: 180px; min-width: 95px; border-radius: 5%; opacity: 0.8;"
+        >
+          <VImg
+            class="ma-3"
+            max-width="100"
+            :src="csvimg"
+          />
+          <p class="text-center text-caption px-2">
+            ASerious_JUN_2023_Sales_Report
+          </p>
+        </VCard>
+
+        <VCard
+          class="elevation-0 ma-1"
+          style="max-width: 10%; max-height: 180px; min-width: 95px; border-radius: 5%; opacity: 0.8;"
+        >
+          <VImg
+            class="ma-3"
+            max-width="100"
+            :src="csvimg"
+          />
+          <p class="text-center text-caption px-2">
+            ASerious_DEC_2022_Sales_Report
+          </p>
+        </VCard>
+      </VRow>
+    </vcontainer>
+      
+    <div class="text-center">
+      <VDialog
+        v-model="sheet"
+        activator="parent"
+        width="60%"
+      >
+        <VCard class="pa-10">
+          <VContainer
+            class="pa-5 rounded-lg mt-2"
+            style=" background-color: rgb(222, 222, 222);"
+          > 
+            <!--
+              <div class="text-overline text-start ml-10">
+              File Menu :
+              </div> 
+            -->
+            <div class="text-overline text-start ml-10">
+              File Upload :
+            </div>
+            <VFileInput
+              counter
+              truncate-length="15"
+            />
+          </VContainer>
+          <VBtn
+            class="mt-5"
+            prepend-icon="mdi-send-variant"
+          >
+            Button
+          </VBtn>
+        </VCard>
+      </VDialog>
+    </div>
+  </div>
+</template>
---
+++ src/views/pages/account-settings/AccountSettingsWhatsapp.vue
@@ -0,0 +1,130 @@
+<script setup>
+const recentDevices = ref([
+  {
+    type: '60177824922',
+    email: true,
+    browser: true,
+    app: true,
+  },
+  {
+    type: '60126969001',
+    email: true,
+    browser: true,
+    app: true,
+  },
+  {
+    type: '601132871109',
+    email: true,
+    browser: true,
+    app: false,
+  },
+  {
+    type: '60148888991',
+    email: true,
+    browser: false,
+    app: false,
+  },
+])
+
+const selectedNotification = ref('Only when I\'m online')
+</script>
+
+<template>
+  <VCard title="Recent Devices">
+    <VCardText>
+      We need permission to provide access to specific user who allow to use SuDu whatsapp.
+      <a href="javascript:void(0)">Learn More</a>
+    </VCardText>
+
+    <VTable class="text-no-wrap">
+      <thead>
+        <tr>
+          <th scope="col">
+            Contact Number (6017*******)
+          </th>
+          <th scope="col">
+            Active 
+          </th>
+          <th scope="col">
+            Action
+          </th>
+          <!--
+            <th scope="col">
+            App
+            </th> 
+          -->
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="device in recentDevices"
+          :key="device.type"
+        >
+          <td>
+            {{ device.type }}
+          </td>
+          <!--
+            <td>
+            <VCheckbox v-model="device.email" />
+            </td>
+          -->
+          <td>
+            <VCheckbox v-model="device.browser" />
+          </td> 
+          <td>
+            <VBtn
+              icon
+              size="25"
+              color="error"
+            >
+              <VIcon size="20">
+                mdi-delete
+              </VIcon>
+            </VBtn>
+          </td>
+        </tr>
+      </tbody>
+    </VTable>
+    <VDivider />
+
+    <VCardText>
+      <VForm @submit.prevent="() => {}">
+        <p class="text-base font-weight-medium">
+          Add new contact to allow whatsapp user
+        </p>
+
+        <VRow>
+          <VCol
+            cols="12"
+            sm="6"
+          >
+            <VTextField
+              label="6012*******"
+              variant="solo-filled"
+            />
+            <!--
+              <VSelect
+              v-model="selectedNotification"
+              mandatory
+              :items="['Only when I\'m online', 'Anytime']"
+              /> 
+            -->
+          </VCol>
+        </VRow>
+
+        <div class="d-flex flex-wrap gap-4 mt-4">
+          <VBtn type="submit">
+            Add Contact 
+          </VBtn>
+          <VBtn
+            color="secondary"
+            variant="tonal"
+            type="reset"
+          >
+            Reset
+          </VBtn>
+        </div>
+      </VForm>
+    </VCardText>
+  </VCard>
+</template>
